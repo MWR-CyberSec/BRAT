7c95de36761794d08c3e6df57ba5d44c42738e04
Ethan <ethanhavinga@gmail.com>
2025-03-18 19:24:14 +0200

    Implement policies and add them to the enforcer

--- router/route.go
+++ router/route.go
@@ -2,14 +2,35 @@ package router
 
 import (
 	"github.com/Et43/BARK/config"
+	"github.com/casbin/casbin/v2"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(init *config.Initialization) *gin.Engine {
 
+	db := config.InitDB()
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+
+	enforcer, err := casbin.NewEnforcer("config/rbac_model.conf", db)
+	if err != nil {
+		panic(err)
+	}
+
+	// Policies
+	if hasPolicy, err := enforcer.HasPolicy("admin", "/api/user", "(GET)|(POST)|(PUT)|(DELETE)|(PATCH)"); !hasPolicy {
+		enforcer.AddPolicy("admin", "/api/user", "(GET)|(POST)|(PUT)|(DELETE)|(PATCH)")
+	} else if err != nil {
+		panic(err)
+	}
+
+	if hasPolicy, err := enforcer.HasPolicy("user", "/api/user", "GET"); !hasPolicy {
+		enforcer.AddPolicy("admin", "/api/user", "GET")
+	} else if err != nil {
+		panic(err)
+	}
 
 	api := router.Group("/api")
 	{
